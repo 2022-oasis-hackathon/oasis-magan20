01e02870a3a4fc94c15524357950bb0adc7c277b
cosmos-1885 <dmsrkql104321@gamil.com>
2022-07-20 15:13:01 +0900

    Create Main, List, Detail, MyPage

--- app/src/main/java/com/example/oasis_project/MyPageActivity.java
+++ app/src/main/java/com/example/oasis_project/MyPageActivity.java
@@ -1,7 +1,10 @@
 package com.example.oasis_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyPageActivity extends AppCompatActivity {
 
